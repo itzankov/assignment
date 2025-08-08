fd08e98402814714a131eb4bc6b2ea1c29637a23
Ilian Tzankov <ilian@tzankov.eu>
2025-08-08 16:52:03 +0300

    Initial

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from langchain.agents.agent_types import AgentType
 from langchain.tools import tool
 from langchain.callbacks.tracers.langchain import LangChainTracer
 import train
+import pandas as pd
 
-model = train.train_model()
+recomender_model = train.train_model()
 
+life_expectancy_data = pd.read_csv("life_expectancy.csv")
 
 # Load environment variables
 os.environ["LANGCHAIN_API_KEY"] = ""
@@ -18,23 +20,38 @@ os.environ["OPENAI_API_KEY"] = ""
 
 app = FastAPI()
 
+def numeric_sex(sex):
+    if sex.lower()[0] == "f":
+        return 0
+    else:
+        return 1
+
 # Define a simple tool
 @tool
-def recommend(age: int, sex: str) -> str:
-    """recommend tv show."""
+def recommend_tv_show(age: int, sex: str) -> str:
+    """recommend tv show based on age and sex"""
     try:
-        if sex.lower() [0] == "m":
-            user_sex = 1
-        else
-            user_sex = 0;
-        result = model.predict([age, user_sex]);
+        result = recomender_model.predict([age, numeric_sex(sex)]);
         return f"Result: {result[0]}"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+@tool
+def life_expectancy(age: int, sex: str) -> str:
+    """Alculate life expectancy based on age and sex"""
+    try:
+        if age < 0 or age > 119:
+            return f"age must be between 0 and 119"
+        age_index = age;
+        if not numeric_sex(sex):
+            age_index += 120
+        return life_expectancy_data.iloc[age_index][4]
     except Exception as e:
         return f"Error: {str(e)}"
 
 # Initialize LangChain agent with LangSmith tracing
 llm = ChatOpenAI(temperature=0)
-tools = [recommend]
+tools = [recommend_tv_show, life_expectancy]
 tracer = LangChainTracer(project_name=os.environ["LANGCHAIN_PROJECT"])
 agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True, callbacks=[tracer])
 
